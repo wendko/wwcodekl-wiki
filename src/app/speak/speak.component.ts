2a620d8c41200a2e040976af34bd976ab4a2dd24
wendko <wendko.88@gmail.com>
2019-08-11 01:04:44 +0800

    adding new files

---
+++ src/app/speak/speak.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speak',
+  templateUrl: './speak.component.html',
+  styleUrls: ['./speak.component.scss']
+})
+export class SpeakComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
